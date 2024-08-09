8ef1c8d49807cba8eeb2ca1ba090b3c7582a758c
oleksandr_taruraiev <oleksandr_taruraiev@epam.com>
2024-08-09 21:11:33 +0300

    update4

--- target/main.d
+++
@@ -1,3 +0,0 @@
-target/main.o: src/main.cpp include/HttpServer.h include/microhttpd.h
-include/HttpServer.h:
-include/microhttpd.h:
--- target/test_main.d
+++
@@ -1,4 +0,0 @@
-target/test_main.o: tests/test_main.cpp tests/../include/HttpServer.h \
- include/microhttpd.h
-tests/../include/HttpServer.h:
-include/microhttpd.h:
